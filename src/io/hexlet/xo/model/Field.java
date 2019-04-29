39e39c0174f2c638dcd917d5c407aab1ddf1c660
ptNIKO <simonich2016@gmail.com>
2019-04-29 23:38:44 +0300

    Field - created

---
+++ src/io/hexlet/xo/model/Field.java
@@ -0,0 +1,46 @@
+package io.hexlet.xo.model;
+
+import io.hexlet.xo.model.Exception.AlreadyOccupiedException;
+import io.hexlet.xo.model.Exception.InvalidPointException;
+
+import java.awt.*;
+
+public class Field {
+    private static final int FIELD_SIZE = 3;
+
+    private static final int MIN_CORDINATE = 0;
+
+    private static final int MAX_CORDINATE = FIELD_SIZE;
+
+    private final Figure[][] field = new Figure[FIELD_SIZE][FIELD_SIZE];
+
+    public int getSize(){
+        return FIELD_SIZE;
+    }
+
+    public Figure getFigure(final Point point) throws InvalidPointException {
+        if (!checkPoint(point)){
+            throw new InvalidPointException();
+        }
+        return field[point.x][point.y];
+    }
+
+    public void setFigure(final Point point,final Figure figure) throws InvalidPointException,AlreadyOccupiedException {
+        if (!checkPoint(point)){
+            throw new InvalidPointException();
+        }
+        if (field[point.x][point.y] != null){
+            throw new AlreadyOccupiedException();
+        }
+
+        field[point.x][point.y] = figure;
+    }
+
+    private boolean checkPoint (final Point point){
+        return checkCordinate(point.x) && checkCordinate(point.y);
+    }
+
+    private boolean checkCordinate(final int cordinate){
+        return cordinate >= MIN_CORDINATE && cordinate < MAX_CORDINATE;
+    }
+}
