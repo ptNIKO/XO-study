78e010c6173166391b240b0534a0f7c806166420
ptNIKO <simonich2016@gmail.com>
2019-05-01 18:56:38 +0300

    WinnerController tests

---
+++ tst/io/hexlet/xo/controllers/WinnerControllerTest.java
@@ -0,0 +1,24 @@
+package io.hexlet.xo.controllers;
+
+import io.hexlet.xo.model.Field;
+import io.hexlet.xo.model.Figure;
+import org.junit.Test;
+
+import java.awt.*;
+
+import static org.junit.Assert.*;
+
+public class WinnerControllerTest {
+
+    @Test
+    public void getWinnerWhenWinnerRow() throws Exception{
+        final WinnerController winnerController = new WinnerController();
+        for (int i = 0;i < 3;i++){
+            final Field field = new Field(3);
+            field.setFigure(new Point(i,0), Figure.X);
+            field.setFigure(new Point(i,1), Figure.X);
+            field.setFigure(new Point(i,2), Figure.X);
+            assertEquals(Figure.X,winnerController.getWinner(field));
+        }
+    }
+}
